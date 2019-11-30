c2d67cb7e07628a4a8939629b39f345cbc2ad05d
dylanharper <harper.dylan@gmail.com>
2019-11-30 11:46:20 -0800

    adding twitter_faves processor

--- husker.py
+++ husker.py
@@ -24,17 +24,48 @@ def _upload_data(bucket_name: str, blob_name: str, data: str):
     blob.upload_from_string(data)
 
 
+def twitter_faves(blob_name: str):
+    """Process raw response data from the catalog at exoplanet.eu."""
+    prefix, storage_key, file_name = blob_name.split('/')
+    collected_timestamp = file_name.split('.')[0]
+
+    raw_str = _download_blob_as_str(blob_name)
+
+    tweets = json.loads(raw_str)
+
+    for tweet in tweets:
+        output_data = {'husker_processor': 'twitter_faves',
+                       'husker_data_version': '0',
+                       'data_source': 'Twitter API',
+                       'collected_timestamp': collected_timestamp,
+                       'text': tweet.get('full_text') or tweet.get('text'),
+                       'id': tweet['id'],
+                       'created_at': parse(tweet['created_at']).strftime('%Y-%m-%dT%H:%M:%SZ'),
+                       'user_id': tweet['user']['id'],
+                       'user_screen_name': tweet['user']['screen_name'],
+                       'user_followers_count': tweet['user']['followers_count'],
+                       'retweet_count': tweet['retweet_count'],
+                       'favorite_count': tweet['favorite_count']}
+
+        output_blob_name = f'{prefix}/{storage_key}/{tweet['id']}.{collected_timestamp}.json'
+
+        _upload_data('dotufp-data', output_blob_name, json.dumps(output_data))
+
 def planets_data_eu(blob_name: str):
     """Process raw response data from the catalog at exoplanet.eu."""
+    prefix, storage_key, file_name = blob_name.split('/')
+    collected_timestamp = file_name.split('.')[0]
+
     raw_str = _download_blob_as_str(blob_name)
 
     planets_data = pd.read_csv(io.BytesIO(raw_str))
     num_planets = planets_data.shape[0]
 
     output_data = {'husker_processor': 'planets_data_eu',
-                             'husker_data_version': '0',
-                             'data_source': 'The ExtrasolarPlanets Encyclopaedia',
-                             'num_planets': num_planets}
+                   'husker_data_version': '1',
+                   'collected_timestamp': collected_timestamp,
+                   'data_source': 'The ExtrasolarPlanets Encyclopaedia',
+                   'num_planets': num_planets}
 
     output_blob_name = blob_name.replace('.raw', '.json')
 
@@ -42,15 +73,19 @@ def planets_data_eu(blob_name: str):
 
 def planets_data_nasa(blob_name: str):
     """Process raw response data from the NASA exoplanet archive."""
+    prefix, storage_key, file_name = blob_name.split('/')
+    collected_timestamp = file_name.split('.')[0]
+
     raw_str = _download_blob_as_str(blob_name)
 
     planets_data = pd.read_csv(io.BytesIO(raw_str))
     num_planets = planets_data.shape[0]
 
     output_data = {'husker_processor': 'planets_data_nasa',
-                             'husker_data_version': '0',
-                             'data_source': 'NASA Exoplanet Archive',
-                             'num_planets': num_planets}
+                   'husker_data_version': '1',
+                   'collected_timestamp': collected_timestamp,
+                   'data_source': 'NASA Exoplanet Archive',
+                   'num_planets': num_planets}
 
     output_blob_name = blob_name.replace('.raw', '.json')
 
--- main.py
+++ main.py
@@ -14,10 +14,12 @@ def husker_worker(data, context):
     """
     blob_name = data['name']
     print(f'Blob: {blob_name}')
-    prefix, storage_key, timestamp = blob_name.split('/')
+    prefix, storage_key, file_name = blob_name.split('/')
 
     if prefix == 'web_scrape':
         if storage_key == 'planets_data_eu':
             husker.planets_data_eu(blob_name)
         elif storage_key == 'planets_data_nasa':
             husker.planets_data_nasa(blob_name)
+    elif prefix == 'twitter_faves':
+        husker.twitter_faves(blob_name)
